4f91dabd6867d35b6fe7fa98bdc0fb2c6eb6fd16
Okereke Onyekachi <onyekachi.o@ng.lopworks.com>
2017-06-18 00:31:34 +0100

    FInsihed setting up vue-cli
    Signed-off-by: Okereke Onyekachi <onyekachi.o@ng.lopworks.com>

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <message>Hello there</message>
+  </div>
+</template>
+
+<script>
+import message from './components/message.vue';
+export default {
+  name: 'app',
+  components: {message},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
