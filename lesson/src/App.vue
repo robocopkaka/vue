f03b8322dbe248ef0930b44f3db8f3bc27d53d86
Okereke Onyekachi <onyekachi.o@ng.lopworks.com>
2017-06-18 14:14:40 +0100

    About to start working with axios

---
+++ lesson/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+export default {
+  name: 'app',
+  components: {Counter}
+}
+</script>
---
+++ lesson/src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h1>The count: {{count}}</h1>
+
+		<p>
+			<button @click="count += 1">Increment Count</button>
+		</p>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{count: 0}
+		}
+	}
+</script>
